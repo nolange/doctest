c07266b17a29eccc3d473d1cac518f874b472377
Norbert Lange <nolange79@gmail.com>
2024-10-30 00:47:41 +0100

    fix debugger detection for MacOS

--- doctest/parts/doctest.cpp
+++ doctest/parts/doctest.cpp
@@ -1452,15 +1452,16 @@ namespace detail {
         auto* pos = std::strstr(const_cast<const char*>(buffer), "TracerPid:\t");
         return pos != nullptr && pos[sizeof("TracerPid:\t") - 1] != '0';
     }
-#elif defined(DOCTEST_PLATFORM_MAC) && defined(__apple_build_version__)
+#elif (defined(DOCTEST_PLATFORM_MAC) || defined(CATCH_PLATFORM_IPHONE)) &&                         \
+        defined(__apple_build_version__)
     // The following function is taken directly from the following technical note:
     // https://developer.apple.com/library/archive/qa/qa1361/_index.html
     // Returns true if the current process is being debugged (either
     // running under the debugger or has a debugger attached post facto).
     bool isDebuggerActive() {
-        int        mib[4];
-        kinfo_proc info;
-        size_t     size;
+        int               mib[4];
+        struct kinfo_proc info;
+        std::size_t       size;
         // Initialize the flags so that, if sysctl fails for some bizarre
         // reason, we get a predictable result.
         info.kp_proc.p_flag = 0;
@@ -1472,8 +1473,11 @@ namespace detail {
         mib[3] = getpid();
         // Call sysctl.
         size = sizeof(info);
-        if(sysctl(mib, DOCTEST_COUNTOF(mib), &info, &size, 0, 0) != 0) {
-            std::cerr << "\nCall to sysctl failed - unable to determine if debugger is active **\n";
+        if(sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, nullptr, 0) != 0) {
+#ifndef DOCTEST_CONFIG_NO_INCLUDE_IOSTREAM
+            std::cerr << "\nCall to sysctl failed - unable to determine if debugger is "
+                         "active **\n";
+#endif
             return false;
         }
         // We're being debugged if the P_TRACED flag is set.
